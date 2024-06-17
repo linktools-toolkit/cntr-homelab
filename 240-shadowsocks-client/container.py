7b3b5ad354d5caf2be46b8f6681b5b33ccd7192e
Hu Ji <jihu.hj@alibaba-inc.com>
2024-06-17 14:13:03 +0800

    add shadowsocks-client

---
+++ 240-shadowsocks-client/container.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+@author  : Hu Ji
+@file    : deploy.py 
+@time    : 2023/05/21
+@site    :  
+@software: PyCharm 
+
+              ,----------------,              ,---------,
+         ,-----------------------,          ,"        ,"|
+       ,"                      ,"|        ,"        ,"  |
+      +-----------------------+  |      ,"        ,"    |
+      |  .-----------------.  |  |     +---------+      |
+      |  |                 |  |  |     | -==----'|      |
+      |  | $ sudo rm -rf / |  |  |     |         |      |
+      |  |                 |  |  |/----|`---=    |      |
+      |  |                 |  |  |   ,/|==== ooo |      ;
+      |  |                 |  |  |  // |(((( [33]|    ,"
+      |  `-----------------'  |," .;'| |((((     |  ,"
+      +-----------------------+  ;;  | |         |,"
+         /_)______________(_/  //'   | +---------+
+    ___________________________/___  `,
+   /  oooooooooooooooo  .o.  oooo /,   `,"-----------
+  / ==ooooooooooooooo==.o.  ooo= //   ,``--{)B     ,"
+ /_==__==========__==_ooo__ooo=_/'   /___________,"
+"""
+import os
+
+from linktools import Config
+from linktools.container import BaseContainer
+from linktools.decorator import cached_property
+
+
+class Container(BaseContainer):
+
+    @cached_property
+    def configs(self):
+        return dict(
+            SHADOWSOCKS_CLIENT_TAG="latest",
+            SHADOWSOCKS_CLIENT_PORT=Config.Prompt(default=1080, type=int, cached=True),
+            SHADOWSOCKS_SERVER_HOST=Config.Prompt(cached=True),
+            SHADOWSOCKS_SERVER_PORT=Config.Prompt(type=int, cached=True),
+            SHADOWSOCKS_SERVER_PASSWORD=Config.Prompt(cached=True),
+            SHADOWSOCKS_SERVER_METHOD=Config.Prompt(default="aes-256-gcm", cached=True),
+        )
+
+    def on_starting(self):
+        self.render_template(
+            self.get_path("config.json"),
+            self.get_app_path("config.json", create_parent=True),
+        )
