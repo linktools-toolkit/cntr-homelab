e57fdc54f6a4ed7c4876664b3de7e93020cf7c3b
Hu Ji <669898595@qq.com>
2025-06-02 21:57:00 +0800

    update nextcloud

--- 300-nextcloud/overlay/scripts/update-configs.sh
+++ 300-nextcloud/overlay/scripts/update-configs.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
 
+NC_TRUSTED_DOMAIN_IDX=1
+add_trusted_domain() {
+    php /var/www/html/occ config:system:set trusted_domains $NC_TRUSTED_DOMAIN_IDX --value=$1
+    NC_TRUSTED_DOMAIN_IDX=$((NC_TRUSTED_DOMAIN_IDX+1))
+}
+
+add_trusted_domain $NEXTCLOUD_DOMAIN
+
 if [ -n "$MAINTENANCE_WINDOW_START" ]; then
     php /var/www/html/occ config:system:set maintenance_window_start --type=integer --value="$MAINTENANCE_WINDOW_START"
 fi
@@ -17,9 +25,12 @@ if [ "$ONLYOFFICE_ENABLED" = 'yes' ]; then
     elif [ "$SKIP_UPDATE" != 1 ]; then
         php /var/www/html/occ app:update onlyoffice
     fi
-    php /var/www/html/occ config:system:set onlyoffice jwt_secret --value="$ONLYOFFICE_SECRET"
     php /var/www/html/occ config:app:set onlyoffice jwt_secret --value="$ONLYOFFICE_SECRET"
-    php /var/www/html/occ config:system:set onlyoffice jwt_header --value="AuthorizationJwt"
+    php /var/www/html/occ config:app:set onlyoffice jwt_header --value="AuthorizationJwt"
     php /var/www/html/occ config:app:set onlyoffice DocumentServerUrl --value="$OVERWRITECLIURL/onlyoffice"
     php /var/www/html/occ config:app:set onlyoffice DocumentServerInternalUrl --value="http://nextcloud-onlyoffice/"
+    php /var/www/html/occ config:app:set onlyoffice StorageUrl --value="http://nextcloud/"
+
+    add_trusted_domain nextcloud
+    add_trusted_domain nextcloud-onlyoffice
 fi
